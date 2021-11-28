8442d73031b565ea9a9fc4f1c1b2da2e012add74
JayLi2018 <lchenjie0302@gmail.com>
2021-11-28 17:12:32 -0600

    sklearn version change

--- setup.py
+++ setup.py
@@ -17,8 +17,7 @@ setup(
                     'networkx==2.5',
                     'pandas==1.3.2',
                     'numpy==1.19.2',
-                    'colorful==0.5.4',
-                    'scikit_learn==0.23.2'],
+                    'colorful==0.5.4'],
     entry_points={
         'console_scripts': [
             'cajadexplain=src.experiments:main',
