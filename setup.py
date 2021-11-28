d3df7cec8ea66db6fa7dc67dfc129f0b95df44ac
JayLi2018 <lchenjie0302@gmail.com>
2021-11-28 17:27:35 -0600

    update dockerfile

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=['seaborn==0.11.0',
                     'scipy==1.5.2',
                     'Flask==2.0.1',
-                    'psycopg2-binary==2.8.4',
+                    'psycopg2-binary',
                     'varclushi==0.1.0',
                     'matplotlib==3.2.1',
                     'networkx==2.5',
