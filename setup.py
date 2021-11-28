8eff00f48f7bb37d57598b0de4a292f3540b0616
JayLi2018 <lchenjie0302@gmail.com>
2021-11-28 15:54:23 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CaJade',
+    version='0.0.1',
+    url='https://github.com/IITDBGroup/CaJaDe',
+    author='Chenjie Li',
+    author_email='cli112@hawk.iit.edu',
+    description='Description of my package',
+    packages=find_packages('src'),    
+    install_requires=['seaborn==0.11.0',
+                    'scipy==1.7.1',
+                    'Flask==2.0.1',
+                    'psycopg2==2.8.4',
+                    'varclushi==0.1.0',
+                    'matplotlib==3.2.1',
+                    'networkx==2.5',
+                    'pandas==1.3.2',
+                    'numpy==1.19.2',
+                    'colorful==0.5.4',
+                    'scikit_learn==1.0'],
+    entry_points={
+        'console_scripts': [
+            'cajadexplain=src.experiments:main',
+        ]},
+    )
