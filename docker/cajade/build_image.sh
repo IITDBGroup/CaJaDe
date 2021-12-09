e6440802c27d80d76e1b26ae93b2ece26a72f7b4
JayLi2018 <lchenjie0302@gmail.com>
2021-12-09 15:37:35 -0600

    12.9 debussy

--- docker/cajade/build_image.sh
+++ docker/cajade/build_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker build --no-cache -f ./Dockerfile -t 2021-sigmod-reproducibility-cajade_systems:latest .
-# sudo docker push 2021-sigmod-reproducibility-cajade_systems:latest
+sudo docker build --no-cache -f ./Dockerfile -t jayli2018/2021-sigmod-reproducibility-cajade_systems:latest .
+sudo docker push jayli2018/2021-sigmod-reproducibility-cajade_systems:latest
--- docker/postgres/load_dumps.sh
+++ docker/postgres/load_dumps.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-psql -p 5432 -U cajade -d nba01 < ../dumpfile/reproduce_nba01.sql
-psql -p 5432 -U cajade -d nba05 < ../dumpfile/reproduce_nba05.sql
-psql -p 5432 -U cajade -d nba < ../dumpfile/reproduce_nba1.sql
-psql -p 5432 -U cajade -d nba2 < ../dumpfile/reproduce_nba2.sql
-psql -p 5432 -U cajade -d nba4  < ../dumpfile/reproduce_nba4.sql
-psql -p 5432 -U cajade -d nba8  < ../dumpfile/reproduce_nba8.sql
+psql -p 5432 -U cajade -d nba01 < ../datafiles/reproduce_nba01.sql
+psql -p 5432 -U cajade -d nba05 < ../datafiles/reproduce_nba05.sql
+psql -p 5432 -U cajade -d nba < ../datafiles/reproduce_nba1.sql
+psql -p 5432 -U cajade -d nba2 < ../datafiles/reproduce_nba2.sql
+psql -p 5432 -U cajade -d nba4  < ../datafiles/reproduce_nba4.sql
+psql -p 5432 -U cajade -d nba8  < ../datafiles/reproduce_nba8.sql
+psql -p 5432 -U cajade -d nba_cape  < ../datafiles/reproduce_cape.sql
