893bbb4159e232ede3a01cb2881500d6a75b9ee0
JayLi2018 <lchenjie0302@gmail.com>
2021-12-11 12:15:57 -0600

    clean up

--- src/exp_workloads.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-iters=(5)
-
-for i in ${iters[@]}
-	do
-		python3 experiments.py -M 3 -p jape -U japerev -P 5433 -d nba_original -t s -m 0 -F 0.3 -D april_5_workload_rep_${i} -W true
-		python3 experiments.py -M 3 -p jape -U japerev -P 5433 -d mimic_original -t s -m 0 -F 0.3 -D april_5_workload_rep_${i} -W true
-	done
