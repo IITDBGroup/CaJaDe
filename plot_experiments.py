3975f5d6ed92182b56cbdffd0b782bdf3a3e5d2b
lordpretzel <bglavic@iit.edu>
2022-10-27 04:14:45 -0500

    Juseung - updated plot_experiments.py results

--- plot_experiments.py
+++ plot_experiments.py
@@ -7,45 +7,45 @@ cur = conn.cursor()
 ############################################
 ############################################
 # ## Plot for CaJaDe_prev - show the testing results in a single plot
-# prev_jg_x = []
-# prev_jg_y = []
-# prev_ptt_x = []
-# prev_ptt_y = []
-# prev_jg_total_time = 0
+#prev_jg_x = []
+#prev_jg_y = []
+#prev_ptt_x = []
+#prev_ptt_y = []
+#prev_jg_total_time = 0
 
 # # runtime in the Join Graph generation
-# get_prev_jg_results = "SELECT jg_enumeration, jg_hashing, jg_validtaion FROM exp_2022_10_24_03_47_04.time_and_params;"
-# cur.execute(get_prev_jg_results)
-# prev_jg_results = cur.fetchall()
+#get_prev_jg_results = "SELECT jg_enumeration, jg_hashing, jg_validtaion FROM exp_2022_10_24_03_47_04.time_and_params;"
+#cur.execute(get_prev_jg_results)
+#prev_jg_results = cur.fetchall()
 
-# for item in prev_jg_results[0]:
-#   prev_jg_total_time+=float(item)
-# prev_jg_total_time = round(prev_jg_total_time)
+#for item in prev_jg_results[0]:
+#  prev_jg_total_time+=float(item)
+#prev_jg_total_time = round(prev_jg_total_time)
 
-# for i in range(0,prev_jg_total_time+1):
-#   prev_jg_x.append(i)
-#   prev_jg_y.append(0)
+#for i in range(0,prev_jg_total_time+1):
+#  prev_jg_x.append(i)
+#  prev_jg_y.append(0)
 
-# plt.plot(prev_jg_x, prev_jg_y, 'o--')
+#plt.plot(prev_jg_x, prev_jg_y, 'o--')
 
 # # runtime in the Pattern generation
-# get_prev_ptt_results = "SELECT * FROM exp_2022_10_24_03_47_04.cajade_prev_testing;"
-# cur.execute(get_prev_ptt_results)
-# prev_ptt_results = cur.fetchall()
+#get_prev_ptt_results = "SELECT * FROM exp_2022_10_24_03_47_04.cajade_prev_testing;"
+#cur.execute(get_prev_ptt_results)
+#prev_ptt_results = cur.fetchall()
 
-# for item in prev_ptt_results:
-#   prev_ptt_x.append(item[0]+prev_jg_total_time)
-#   prev_ptt_y.append(item[1])
+#for item in prev_ptt_results:
+#  prev_ptt_x.append(item[0]+prev_jg_total_time)
+#  prev_ptt_y.append(item[1])
   
-# plt.plot(prev_ptt_x, prev_ptt_y, 'o-')
+#plt.plot(prev_ptt_x, prev_ptt_y, 'o-')
 
-# plt.xlabel('time(sec)')
-# plt.ylabel('# explanation')
-# plt.title('Runtime experiment of the CaJaDE system')
+#plt.xlabel('time(sec)')
+#plt.ylabel('# explanation')
+#plt.title('Runtime experiment of the CaJaDE system')
 # #plt.show()
-# plt.savefig('prev_result.png')
+#plt.savefig('prev_result.png')
 
-# conn.commit()
+#conn.commit()
 
 ############################################
 ############################################
@@ -54,11 +54,15 @@ rate_list = ['10', '09', '08', '07', '06', '05']
 stop_list = [1, 3, 5, 7, 9]
 plot_option = ['bo', 'gs', 'rv', 'cp', 'm*']
 
+sub_plots = plt.subplots(2,3,figsize=(15,15))
+fig = sub_plots[0]
+graph = sub_plots[1]
+
 # select * from testing_05_1.cajade_new_testing;
 # select jg_e_cum, jg_h_cum, jg_v_cum, jg_s_cum, jg_utime_cum from testing_10_1.time_and_params;
 
 for k in range(0,len(rate_list)):
-  plt.subplot(2,3,k+1)
+  #plt.subplot(2,3,k+1)
   for i in range(0,len(stop_list)):
     new_jg_x = []
     new_jg_y = []
@@ -80,7 +84,10 @@ for k in range(0,len(rate_list)):
       new_jg_x.append(j)
       new_jg_y.append(0)
 
-    plt.plot(new_jg_x, new_jg_y, plot_option[i]+'--')
+    if k<3:
+      graph[0][k].plot(new_jg_x, new_jg_y, plot_option[i]+'--')
+    else:
+      graph[1][k-3].plot(new_jg_x, new_jg_y, plot_option[i]+'--')
 
     # runtime in the Pattern generation
     get_new_ptt_results = "SELECT * FROM "+schema_name+".cajade_new_testing;"
@@ -91,12 +98,25 @@ for k in range(0,len(rate_list)):
       new_ptt_x.append(item[0]+new_jg_total_time)
       new_ptt_y.append(item[1])
 
-    plt.plot(new_ptt_x, new_ptt_y, plot_option[i]+'-')
+    if k<3:
+      #graph[0][k].figure(figsize=(10,5))
+      graph[0][k].plot(new_ptt_x, new_ptt_y, plot_option[i]+'-')
+      graph[0][k].set_title(schema_name+' result')
+      graph[0][k].set_xlabel('time(sec)')
+      graph[0][k].set_ylabel('# explanation')
+    else:
+      #graph[1][k].figure(figsize=(10,5))
+      graph[1][k-3].plot(new_ptt_x, new_ptt_y, plot_option[i]+'-')
+      graph[1][k-3].set_title(schema_name+' result')
+      graph[1][k-3].set_xlabel('time(sec)')
+      graph[1][k-3].set_ylabel('# explanation')
     
-plt.xlabel('time(sec)')
-plt.ylabel('# explanation')
-plt.title('Runtime experiment of the CaJaDE system')
-plt.savefig('new_result.png')
+#plt.xlabel('time(sec)')
+#plt.ylabel('# explanation')
+#plt.title('Runtime experiment of the CaJaDE system')
+fig.suptitle('Runtime experiment of the new approach')
+fig.tight_layout(pad=2)
+plt.savefig('new_results.png')
 conn.commit()
 
 
