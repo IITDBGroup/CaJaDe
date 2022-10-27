70664359e80b0ba65544f66657e7b8187848e795
lordpretzel <bglavic@iit.edu>
2022-10-27 14:34:20 -0500

    Juseung - updated

--- plot_experiments.py
+++ plot_experiments.py
@@ -49,32 +49,32 @@ cur = conn.cursor()
 
 ############################################
 ############################################
-## Plot for CaJaDe_new - Each # stop has six different rates
+## Plot for CaJaDe_new - Each # rates has five different stops
 rate_list = ['10', '09', '08', '07', '06', '05']
 stop_list = [1, 3, 5, 7, 9]
-marker_shape = ['o', 's', 'v', 'p', '*','D']
-marker_color = ['b','g','r','c','m','k']
+marker_shape = ['o', 's', 'v', 'p', '*']
+marker_color = ['b','g','r','c','m']
 
-#sub_plots = plt.subplots(2,3,figsize=(15,15))
-#fig = sub_plots[0]
-#graph = sub_plots[1]
+sub_plots = plt.subplots(2,3,figsize=(30,15))
+fig = sub_plots[0]
+graph = sub_plots[1]
 
 # select * from testing_05_1.cajade_new_testing;
 # select jg_e_cum, jg_h_cum, jg_v_cum, jg_s_cum, jg_utime_cum from testing_10_1.time_and_params;
 
-for k in range(0,len(stop_list)): 
-  #plt.subplot(2,3,k+1)
-  sub_plots = plt.subplots(2,3,figsize=(15,15))
-  fig = sub_plots[0]
-  graph = sub_plots[1]
+for k in range(0,len(rate_list)): 
+#k = rate_list[0]
+#sub_plots = plt.subplots(2,3,figsize=(30,15))
+#fig = sub_plots[0]
+#graph = sub_plots[1]
 
-  for i in range(0,len(rate_list)):
+  for i in range(0,len(stop_list)):
     new_jg_x = []
     new_jg_y = []
     new_ptt_x = []
     new_ptt_y = []
     new_jg_total_time = 0
-    schema_name = "testing_"+str(rate_list[i])+"_"+str(stop_list[k])
+    schema_name = "testing_"+str(rate_list[k])+"_"+str(stop_list[i])
 
     # runtime in the Join Graph generation
     get_new_jg_results = "SELECT jg_e_cum, jg_h_cum, jg_v_cum, jg_s_cum, jg_utime_cum FROM "+schema_name+".time_and_params;"
@@ -104,25 +104,23 @@ for k in range(0,len(stop_list)):
       new_ptt_y.append(item[1])
 
     if k<3:
-      #graph[0][k].figure(figsize=(10,5))
-      graph[0][k].plot(new_ptt_x, new_ptt_y, marker_color[i]+marker_shape[i]+'-')
-      graph[0][k].set_title('stop #'+str(stop_list[k])+' result')
+      graph[0][k].plot(new_ptt_x, new_ptt_y, marker_color[i]+marker_shape[i]+'-',label='stop #'+str(stop_list[i]))
+      graph[0][k].set_title('rate_'+str(rate_list[k])+'_result')
       graph[0][k].set_xlabel('time(sec)')
       graph[0][k].set_ylabel('# explanation')
     else:
-      #graph[1][k].figure(figsize=(10,5))
-      graph[1][k-3].plot(new_ptt_x, new_ptt_y, marker_color[i]+marker_shape[i]+'-')
-      graph[1][k-3].set_title('stop #'+str(stop_list[k])+' result')
+      graph[1][k-3].plot(new_ptt_x, new_ptt_y, marker_color[i]+marker_shape[i]+'-',label='stop #'+str(stop_list[i]))
+      graph[1][k-3].set_title('rate_'+str(rate_list[k])+' result')
       graph[1][k-3].set_xlabel('time(sec)')
       graph[1][k-3].set_ylabel('# explanation')
     
 #plt.xlabel('time(sec)')
 #plt.ylabel('# explanation')
 #plt.title('Runtime experiment of the CaJaDE system')
-  fig.suptitle('Runtime experiment of the new approach')
-  fig.tight_layout(pad=2)
-  plt.savefig('stop_'+str(stop_list[k])+'_results.png')
-  conn.commit()
+fig.suptitle('Runtime experiment of the new approach')
+fig.tight_layout(pad=5)
+plt.savefig('new_result.png')
+conn.commit()
 
 
 
