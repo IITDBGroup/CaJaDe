b7c45e9c2caca64d6ae78cfe34081c38fb035808
lordpretzel <bglavic@iit.edu>
2022-10-27 13:54:53 -0500

    Juseung - updated plot_experiments.py results

--- plot_experiments.py
+++ plot_experiments.py
@@ -21,12 +21,12 @@ cur = conn.cursor()
 #for item in prev_jg_results[0]:
 #  prev_jg_total_time+=float(item)
 #prev_jg_total_time = round(prev_jg_total_time)
-
+#print(prev_jg_total_time)
 #for i in range(0,prev_jg_total_time+1):
 #  prev_jg_x.append(i)
 #  prev_jg_y.append(0)
 
-#plt.plot(prev_jg_x, prev_jg_y, 'o--')
+#plt.plot(prev_jg_x, prev_jg_y, '--')
 
 # # runtime in the Pattern generation
 #get_prev_ptt_results = "SELECT * FROM exp_2022_10_24_03_47_04.cajade_prev_testing;"
@@ -37,7 +37,7 @@ cur = conn.cursor()
 #  prev_ptt_x.append(item[0]+prev_jg_total_time)
 #  prev_ptt_y.append(item[1])
   
-#plt.plot(prev_ptt_x, prev_ptt_y, 'o-')
+#plt.plot(prev_ptt_x, prev_ptt_y, '-')
 
 #plt.xlabel('time(sec)')
 #plt.ylabel('# explanation')
@@ -49,27 +49,32 @@ cur = conn.cursor()
 
 ############################################
 ############################################
-## Plot for CaJaDe_new - Each # rate has five different stop
+## Plot for CaJaDe_new - Each # stop has six different rates
 rate_list = ['10', '09', '08', '07', '06', '05']
 stop_list = [1, 3, 5, 7, 9]
-plot_option = ['bo', 'gs', 'rv', 'cp', 'm*']
+marker_shape = ['o', 's', 'v', 'p', '*','D']
+marker_color = ['b','g','r','c','m','k']
 
-sub_plots = plt.subplots(2,3,figsize=(15,15))
-fig = sub_plots[0]
-graph = sub_plots[1]
+#sub_plots = plt.subplots(2,3,figsize=(15,15))
+#fig = sub_plots[0]
+#graph = sub_plots[1]
 
 # select * from testing_05_1.cajade_new_testing;
 # select jg_e_cum, jg_h_cum, jg_v_cum, jg_s_cum, jg_utime_cum from testing_10_1.time_and_params;
 
-for k in range(0,len(rate_list)):
+for k in range(0,len(stop_list)): 
   #plt.subplot(2,3,k+1)
-  for i in range(0,len(stop_list)):
+  sub_plots = plt.subplots(2,3,figsize=(15,15))
+  fig = sub_plots[0]
+  graph = sub_plots[1]
+
+  for i in range(0,len(rate_list)):
     new_jg_x = []
     new_jg_y = []
     new_ptt_x = []
     new_ptt_y = []
     new_jg_total_time = 0
-    schema_name = "testing_"+str(rate_list[k])+"_"+str(stop_list[i])
+    schema_name = "testing_"+str(rate_list[i])+"_"+str(stop_list[k])
 
     # runtime in the Join Graph generation
     get_new_jg_results = "SELECT jg_e_cum, jg_h_cum, jg_v_cum, jg_s_cum, jg_utime_cum FROM "+schema_name+".time_and_params;"
@@ -85,9 +90,9 @@ for k in range(0,len(rate_list)):
       new_jg_y.append(0)
 
     if k<3:
-      graph[0][k].plot(new_jg_x, new_jg_y, plot_option[i]+'--')
+      graph[0][k].plot(new_jg_x, new_jg_y, marker_color[i]+'--')
     else:
-      graph[1][k-3].plot(new_jg_x, new_jg_y, plot_option[i]+'--')
+      graph[1][k-3].plot(new_jg_x, new_jg_y, marker_color[i]+'--')
 
     # runtime in the Pattern generation
     get_new_ptt_results = "SELECT * FROM "+schema_name+".cajade_new_testing;"
@@ -100,24 +105,24 @@ for k in range(0,len(rate_list)):
 
     if k<3:
       #graph[0][k].figure(figsize=(10,5))
-      graph[0][k].plot(new_ptt_x, new_ptt_y, plot_option[i]+'-')
-      graph[0][k].set_title(schema_name+' result')
+      graph[0][k].plot(new_ptt_x, new_ptt_y, marker_color[i]+marker_shape[i]+'-')
+      graph[0][k].set_title('stop #'+str(stop_list[k])+' result')
       graph[0][k].set_xlabel('time(sec)')
       graph[0][k].set_ylabel('# explanation')
     else:
       #graph[1][k].figure(figsize=(10,5))
-      graph[1][k-3].plot(new_ptt_x, new_ptt_y, plot_option[i]+'-')
-      graph[1][k-3].set_title(schema_name+' result')
+      graph[1][k-3].plot(new_ptt_x, new_ptt_y, marker_color[i]+marker_shape[i]+'-')
+      graph[1][k-3].set_title('stop #'+str(stop_list[k])+' result')
       graph[1][k-3].set_xlabel('time(sec)')
       graph[1][k-3].set_ylabel('# explanation')
     
 #plt.xlabel('time(sec)')
 #plt.ylabel('# explanation')
 #plt.title('Runtime experiment of the CaJaDE system')
-fig.suptitle('Runtime experiment of the new approach')
-fig.tight_layout(pad=2)
-plt.savefig('new_results.png')
-conn.commit()
+  fig.suptitle('Runtime experiment of the new approach')
+  fig.tight_layout(pad=2)
+  plt.savefig('stop_'+str(stop_list[k])+'_results.png')
+  conn.commit()
 
 
 
